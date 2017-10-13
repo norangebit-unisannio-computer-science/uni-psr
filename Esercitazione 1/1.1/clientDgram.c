b5938aa9a1a3158c1d3f196a89ccf46594c49db0
Raffaele Mignone <raffy.m.96@gmail.com>
2017-10-14 00:09:07 +0200

    fix

--- Esercitazione 1/1.1/clientDgram.c
+++ Esercitazione 1/1.1/clientDgram.c
@@ -1,9 +1,9 @@
 /*
- *  clientDgram.c
- *
- *  Modified on: October 9, 2017
- *  Author: zimeo
- */
+*  clientDgram.c
+*
+*  Modified on: October 9, 2017
+*  Author: zimeo
+*/
 
 /*#include <sys/types.h>*/
 #include <sys/socket.h>
@@ -14,46 +14,46 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
-  char buf[256];
-  int clientSocket;
-  struct sockaddr_in remoteAddr;
-  socklen_t remoteAddrLen;
-  char localhost[] = "127.0.0.1"; /* Indirizzo di host di default */
-  char *host;
+	char buf[256];
+	int clientSocket;
+	struct sockaddr_in remoteAddr;
+	socklen_t remoteAddrLen;
+	char localhost[] = "127.0.0.1"; /* Indirizzo di host di default */
+	char *host;
 
-  /* Verifica la presenza dell'indirizzo IP sulla linea di comando */
-  if (argc > 1) {
-          host = argv[1]; /* if host argument specified */
-  } else {
-          host = localhost;
-  }
-  
-  /* Crea una socket per un canale orientato ai datagram */
-  clientSocket = socket(PF_INET, SOCK_DGRAM, 0);
+	/* Verifica la presenza dell'indirizzo IP sulla linea di comando */
+	if (argc > 1) {
+		host = argv[1]; /* if host argument specified */
+	} else {
+		host = localhost;
+	}
 
-  /* Inizializza l'area di memoria destinata ad ospitare l'indirizzo di traporto della socket remota (server) */
-  memset(&remoteAddr, 0, sizeof(remoteAddr));
-	
-  /* Inizializza l'indirizzo di trasporto della socket remota (server) */
-  remoteAddr.sin_family = AF_INET;
-  remoteAddr.sin_port = htons(5193);
-  remoteAddr.sin_addr.s_addr= inet_addr(host);
+	/* Crea una socket per un canale orientato ai datagram */
+	clientSocket = socket(PF_INET, SOCK_DGRAM, 0);
 
-  remoteAddrLen = sizeof(remoteAddr);
+	/* Inizializza l'area di memoria destinata ad ospitare l'indirizzo di traporto della socket remota (server) */
+	memset(&remoteAddr, 0, sizeof(remoteAddr));
 
-  /* Inizializza l'area di memoria destinata ad ospitare i dati da inviare */
-  memset(buf, 0, sizeof(buf));
-  /* Invia i dati (il messaggio non contiene dati utili) sul canale */
-  sendto(clientSocket, buf, sizeof(buf), 0, (struct sockaddr *)&remoteAddr, remoteAddrLen);
+	/* Inizializza l'indirizzo di trasporto della socket remota (server) */
+	remoteAddr.sin_family = AF_INET;
+	remoteAddr.sin_port = htons(5193);
+	remoteAddr.sin_addr.s_addr= inet_addr(host);
 
-  memset(buf, 0, sizeof(buf));
-  /* Riceve dal canale il messaggio inviato dal server; si assume che il server invii i dati in un unico datagram */
-  recvfrom(clientSocket, buf, sizeof(buf), 0, (struct sockaddr *)&remoteAddr, &remoteAddrLen);
+	remoteAddrLen = sizeof(remoteAddr);
 
-  /* Visualizza sullo stdout i dati ricevuti */
-  printf("%s\n", buf);
+	/* Inizializza l'area di memoria destinata ad ospitare i dati da inviare */
+	memset(buf, 0, sizeof(buf));
+	/* Invia i dati (il messaggio non contiene dati utili) sul canale */
+	sendto(clientSocket, buf, sizeof(buf), 0, (struct sockaddr *)&remoteAddr, remoteAddrLen);
 
-  /* Chiude la socket e rilascia le relative risorse */	
-  close(clientSocket);
-  return 0;
+	memset(buf, 0, sizeof(buf));
+	/* Riceve dal canale il messaggio inviato dal server; si assume che il server invii i dati in un unico datagram */
+	recvfrom(clientSocket, buf, sizeof(buf), 0, (struct sockaddr *)&remoteAddr, &remoteAddrLen);
+
+	/* Visualizza sullo stdout i dati ricevuti */
+	printf("%s\n", buf);
+
+	/* Chiude la socket e rilascia le relative risorse */	
+	close(clientSocket);
+	return 0;
 }
--- Esercitazione 1/1.1/serverDgram.c
+++ Esercitazione 1/1.1/serverDgram.c
@@ -1,9 +1,9 @@
 /*
- *  ServerDgram.c
- *
- *  Modified on: October 9, 2017
- *  Author: zimeo
- */
+*  ServerDgram.c
+*
+*  Modified on: October 9, 2017
+*  Author: zimeo
+*/
 
 /*#include <sys/types.h>*/
 #include <sys/socket.h>
@@ -14,44 +14,44 @@
 
 
 int main(int argc, char* argv[]) {
-  char buf[256];
-  int serverSocket;
-  socklen_t clientAddrLen;
-  int visits = 0;
-  struct sockaddr_in serverAddr;
-  struct sockaddr_in clientAddr;
+	char buf[256];
+	int serverSocket;
+	socklen_t clientAddrLen;
+	int visits = 0;
+	struct sockaddr_in serverAddr;
+	struct sockaddr_in clientAddr;
 
-  /* Crea una socket per un canale orientato ai datagram */
-  serverSocket = socket(PF_INET, SOCK_DGRAM, 0);
-  /* Inizializza l'area di memoria che ospita l'indirizzo di trasporto della socket locale (server) */
-  memset(&serverAddr, 0, sizeof(serverAddr));
+	/* Crea una socket per un canale orientato ai datagram */
+	serverSocket = socket(PF_INET, SOCK_DGRAM, 0);
+	/* Inizializza l'area di memoria che ospita l'indirizzo di trasporto della socket locale (server) */
+	memset(&serverAddr, 0, sizeof(serverAddr));
 
-  /* Inizializza l'indirizzo di trasporto della socket server */
-  serverAddr.sin_family = AF_INET;
-  serverAddr.sin_port = htons(5193);
-  serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	/* Inizializza l'indirizzo di trasporto della socket server */
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(5193);
+	serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-  /* Assegna alla socket locale (server) l'indirizzo di trasporto */
-  bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
-  clientAddrLen = sizeof(clientAddr);
+	/* Assegna alla socket locale (server) l'indirizzo di trasporto */
+	bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
+	clientAddrLen = sizeof(clientAddr);
 
-  /* Il cliclo infinito caratterizza l'attesa continua del server */
-  while(1) {
-      /* Inizializza l'area di memoria destinata a memorizzare i dati estratti dal canale */
-      memset(buf, 0, sizeof(buf));
-      /* Riceve i dati dal canale: copia i dati ricevuti dal buffer di sistema nell'area di memoria utente referenziata da buf */
-      recvfrom(serverSocket, buf, sizeof(buf), 0, (struct sockaddr*)&clientAddr, &clientAddrLen);
-      
-      /* Inizio elaborazione del messaggio di richiesta */
-      /* Viene incrementata la variabile di stato visits e prodotto il messaggio di risposta */
-      visits++;
-      memset(buf, 0, sizeof(buf));
-      sprintf(buf,"This server has been contacted %d time%s\n", visits, visits==1 ? "." : "s.");
-      /* Fine elaborazione del messaggio di richiesta */
-      
-      /* Invia i dati nel canale: copia i dati dal buffer nell'area di memoria utente referenziata da buf */
+	/* Il cliclo infinito caratterizza l'attesa continua del server */
+	while(1) {
+		/* Inizializza l'area di memoria destinata a memorizzare i dati estratti dal canale */
+		memset(buf, 0, sizeof(buf));
+		/* Riceve i dati dal canale: copia i dati ricevuti dal buffer di sistema nell'area di memoria utente referenziata da buf */
+		recvfrom(serverSocket, buf, sizeof(buf), 0, (struct sockaddr*)&clientAddr, &clientAddrLen);
 
-      sendto(serverSocket, buf, sizeof(buf), 0, (struct sockaddr *)&clientAddr, clientAddrLen);
-  }
-  return 0;
+		/* Inizio elaborazione del messaggio di richiesta */
+		/* Viene incrementata la variabile di stato visits e prodotto il messaggio di risposta */
+		visits++;
+		memset(buf, 0, sizeof(buf));
+		sprintf(buf,"This server has been contacted %d time%s\n", visits, visits==1 ? "." : "s.");
+		/* Fine elaborazione del messaggio di richiesta */
+
+		/* Invia i dati nel canale: copia i dati dal buffer nell'area di memoria utente referenziata da buf */
+
+		sendto(serverSocket, buf, sizeof(buf), 0, (struct sockaddr *)&clientAddr, clientAddrLen);
+	}
+	return 0;
 }
